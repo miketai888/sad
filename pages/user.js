d13ac626b13f1f453a585de218e42eca96f0f1f6
Mike <miketai888@outlook.com>
2023-05-11 02:32:33 -0700

    initial

---
+++ pages/user.js
@@ -0,0 +1,32 @@
+function UserList({users}) {
+    return(
+        <>
+        <h1>List of Users</h1>
+        {users.map((user) => {
+            return(
+            <div key={user.fixture.id}><p>{user.fixture.id}</p>
+            </div>
+            )})}
+        </>
+    )
+
+}
+
+export async function getStaticProps() {
+    const response = await fetch("https://v3.football.api-sports.io/fixtures?league=39&season=2022&date=2023-05-13", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "v3.football.api-sports.io",
+            "x-rapidapi-key": "214c84a269641c8a0c49c03f16cabd53"
+        }
+    })
+    const data = await response.json()
+    console.log(data.response)
+    return{
+        props:{
+            users:data.response,
+        }, revalidate: 30
+    }
+}
+
+export default UserList
